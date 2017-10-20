71368a560e9208c463f3f480988f1c264112b53a
Paulo McNally <paulomcnally@gmail.com>
2017-10-19 21:39:01 -0600

    Implement found

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+import { createBrowserRouter, HttpError, makeRouteConfig, Redirect, Route } from 'found';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const BrowserRouter = createBrowserRouter({
+  routeConfig: makeRouteConfig(
+    <Route path="/" Component={App}>
+    </Route>
+  ),
+  renderError: ({ error }) => (
+    <div>
+      {error.status === 404 ? 'Not found' : 'Error'}
+    </div>
+  ),
+});
+
+ReactDOM.render(<BrowserRouter />, document.getElementById('root'));
 registerServiceWorker();
