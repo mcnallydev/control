f9dd51f9f5c8745db9c592087c822a523f108ce3
Paulo McNally <paulomcnally@gmail.com>
2017-10-25 00:34:39 -0600

    Personal data init form

---
+++ src/components/master/birthday/styles.js
@@ -0,0 +1,28 @@
+import styled from 'react-emotion';
+
+export const FormInput = styled.div`
+  position: relative;
+`;
+
+export const FormInputDropdown = styled.div`
+  float: left;
+  align-items: flex-start;
+  justify-content: flex-start;
+  width: ${props => props.width};
+  height: 39px;
+  position: relative;
+  margin-right: 15px;
+`;
+
+export const FormInputDropdownLabel = styled.div`
+  cursor: pointer;
+  box-shadow: 0 1px 3px 0 rgba(0,0,0,.2), 0 1px 1px 0 rgba(0,0,0,.14), 0 -1px 1px -1px rgba(0,0,0,.12);
+  border-radius: 2px;
+  background-color: #FFFFFF;
+  position: fixed;
+  overflow: hidden;
+  color: #212121;
+  display: ${props => props.display ? 'none' : 'block'};
+  width: ${props => props.width};
+  padding: 10px 0 10px 15px;
+`;
--- src/views/users/customer/About.js
+++ src/views/users/customer/About.js
@@ -1,7 +1,56 @@
 import React, { PureComponent } from 'react';
+import Card from 'react-md-card';
+import Input from 'react-md-input';
+import styled from 'react-emotion';
 import Profile from '../../../components/user/Profile';
+import Birthday from '../../../components/master/birthday';
+
+export const Section = styled.div`
+  position: relative;
+  overflow: hidden;
+  margin: 0 auto;
+  width: 600px;
+`;
+
+export const Title = styled.h1`
+  margin 20px 20px 0 20px;
+  font-size: 16px;
+`;
+
+export const FormGroup = styled.div`
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 25px;
+`;
+
+export const FormLabel = styled.h1`
+  font-size: 11px;
+  color: #4285f4;
+  padding-top: ${props => props.isInput ? '20px' : '0'};
+`;
+
+export const FormInput = styled.div`
+  float: left;
+  width: ${props => props.width};
+  position: relative;
+`;
 
 class ViewAbout extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      birthday: '',
+      numberOfChildren: '',
+    };
+  }
+
+  onChange = (value, key) => {
+    let stateValue = (value.hasOwnProperty('target')) ? value.target.value : value;
+    this.setState({
+      [key]: stateValue
+    });
+  }
+
   render() {
     return (
       <Profile
@@ -9,7 +58,32 @@ class ViewAbout extends PureComponent {
         tabSelected="3"
         title="Información"
       >
-        <div>About</div>
+        <Section>
+          <Title>Datos Personales</Title>
+          <Card>
+            <FormGroup>
+              <FormLabel>Fecha de nacimiento:</FormLabel>
+              <Birthday identifier="birthday" onChange={this.onChange} />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                label="Nº de Hijos"
+                type="text"
+                value={this.state.numberOfChildren}
+                pattern={'patternInteger'}
+                onChange={ (e) => this.onChange(e, 'numberOfChildren') }
+              />
+            </FormGroup>
+            <FormGroup>
+              <Input
+                label="Dirección"
+                type="text"
+                value={this.state.address}
+                onChange={ (e) => this.onChange(e, 'address') }
+              />
+            </FormGroup>
+          </Card>
+        </Section>
       </Profile>
     );
   }
